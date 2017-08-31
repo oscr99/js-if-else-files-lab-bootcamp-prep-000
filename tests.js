0348382540031a517fd1ebc651d79ab987653141
Orianna Cerato <oricerato@gmail.com>
2017-08-31 16:52:08 +0000

    automatically backed up by learn

--- tests.js
+++ tests.js
@@ -2,17 +2,16 @@ var index = require("./index.js")
 
 if (index.name === "Susan") {
     console.log("The name is correct")
-    console.log(index.name)
 } else {
     console.log("Expected: Susan, Received: "+index.name)
 }
-
+console.log(index.name)
 
 
 
 if (index.height === 70) {
     console.log("The height is correct")
-    console.log(index.height)
 } else {
     console.log("Expected: 70, Received: "+index.height)
 }
+console.log(index.height)
